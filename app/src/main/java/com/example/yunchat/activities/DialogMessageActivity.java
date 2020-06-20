dd0f423557b04be2d605755d64b36613b77a46ea
TheIboy <379837718@qq.com>
2020-06-20 20:24:40 +0800

    pic

--- app/src/main/java/com/example/yunchat/activities/DialogMessageActivity.java
+++ app/src/main/java/com/example/yunchat/activities/DialogMessageActivity.java
@@ -2,5 +2,15 @@ package com.example.yunchat.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
+import com.example.yunchat.R;
+
 public class DialogMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog_message);
+    }
 }
